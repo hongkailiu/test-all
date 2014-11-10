c35162a8d897a4b7749014845123a9e313e5cd0f
liu hongkai <liuhk2@lenovo.com>
2014-11-10 15:14:47 +0800

    a: start a new project

---
+++ trunk/test-all/test-app/src/main/java/com/hongkailiu/test/app/App.java
@@ -0,0 +1,13 @@
+package com.hongkailiu.test.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
