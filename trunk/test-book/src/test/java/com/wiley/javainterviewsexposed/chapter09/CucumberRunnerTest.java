25058202faf7b7b64feef529cc95214053a027fe
hongkailiu <liuhkai@163.com>
2015-02-09 17:14:05 -0500

    java interview: started with chapter9

---
+++ trunk/test-book/src/test/java/com/wiley/javainterviewsexposed/chapter09/CucumberRunnerTest.java
@@ -0,0 +1,15 @@
+package com.wiley.javainterviewsexposed.chapter09;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+
+/**
+ * 
+ * 
+ * hongkai: the name ends with Test for mvn test command
+ *
+ */
+@RunWith(Cucumber.class)
+//@CucumberOptions(plugin = {"pretty"})
+public class CucumberRunnerTest { }
---
+++ trunk/test-book/src/test/resources/com/wiley/javainterviewsexposed/chapter09/CucumberRunnerTest.feature
@@ -0,0 +1,6 @@
+Feature: A simple mathematical BDD test
+
+	Scenario: Two numbers can be added
+		Given the numbers 3 and 6
+		When the numbers are added together
+		Then the result is 9
