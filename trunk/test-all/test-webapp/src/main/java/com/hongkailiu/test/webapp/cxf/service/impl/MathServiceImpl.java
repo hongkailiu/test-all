10e546a214286799a8d2b9ec8eb0cf692b5d7dd6
hongkailiu <liuhkai@163.com>
2015-03-08 12:14:29 -0400

    restful api: unit test: done

--- trunk/test-all/test-webapp/src/main/java/com/hongkailiu/test/webapp/cxf/service/impl/MathServiceImpl.java
+++ trunk/test-all/test-webapp/src/main/java/com/hongkailiu/test/webapp/cxf/service/impl/MathServiceImpl.java
@@ -1,22 +1,23 @@
 package com.hongkailiu.test.webapp.cxf.service.impl;
 
-import javax.jws.WebParam;
 import javax.jws.WebService;
 
 import org.apache.log4j.Logger;
 
 import com.hongkailiu.test.webapp.cxf.service.MathService;
 
-@WebService(endpointInterface = "com.hongkailiu.test.webapp.cxf.service.MathService")
+@WebService(endpointInterface = "com.hongkailiu.test.webapp.cxf.service.MathService", 
+portName = "MathServicePort",
+serviceName = "MathService",
+targetNamespace = "http://hongkailiu.com/test-webapp/services")
 public class MathServiceImpl implements MathService {
-	
+
 	static Logger logger = Logger.getLogger(MathServiceImpl.class);
 
-	
 	@Override
-	public boolean isOdd(@WebParam(name="number") Integer number) {
+	public boolean isOdd(Integer number) {
 		logger.debug("isOdd");
-		
+
 		if (number == null) {
 			return false;
 		}
