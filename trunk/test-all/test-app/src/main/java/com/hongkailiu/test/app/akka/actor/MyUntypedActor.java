cdbc19b3adeb249ca5ff8408c08603dc33e72643
hongkailiu <liuhkai@gmail.com>
2015-04-07 22:43:13 -0400

    akka: started

---
+++ trunk/test-all/test-app/src/main/java/com/hongkailiu/test/app/akka/actor/MyUntypedActor.java
@@ -0,0 +1,19 @@
+package com.hongkailiu.test.app.akka.actor;
+
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+
+/**
+ * Created by hongkailiu on 2015-04-07.
+ */
+public class MyUntypedActor extends UntypedActor {
+    LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+
+    @Override
+    public void onReceive(Object message) throws Exception {
+        log.info("message: "+message);
+    }
+}
+ß
