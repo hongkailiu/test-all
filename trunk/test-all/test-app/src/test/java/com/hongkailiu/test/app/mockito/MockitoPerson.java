a4b5b164cd2b7a87f06fb9cdd38d302b67872ae9
hongkailiu <liuhkai@gmail.com>
2015-04-08 21:47:20 -0400

    lombok; guice

---
+++ trunk/test-all/test-app/src/test/java/com/hongkailiu/test/app/mockito/MockitoPerson.java
@@ -0,0 +1,11 @@
+package com.hongkailiu.test.app.mockito;
+
+import lombok.Getter;
+
+/**
+ * Created by hongkailiu on 2015-04-08.
+ */
+public class MockitoPerson {
+    @Getter
+    private String name;
+}
