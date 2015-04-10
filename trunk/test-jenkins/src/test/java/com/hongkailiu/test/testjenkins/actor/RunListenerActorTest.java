04a60e8b416bf41f0f5a407515401ebaf5e8b6cc
hongkailiu <liuhkai@gmail.com>
2015-04-09 23:45:01 -0400

    add cleaning resource

--- trunk/test-jenkins/src/test/java/com/hongkailiu/test/testjenkins/actor/RunListenerActorTest.java
+++ trunk/test-jenkins/src/test/java/com/hongkailiu/test/testjenkins/actor/RunListenerActorTest.java
@@ -4,35 +4,36 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.testkit.TestActorRef;
 import com.hongkailiu.test.testjenkins.MyMessage;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import scala.concurrent.Await;
 import scala.concurrent.Future;
 import scala.concurrent.duration.Duration;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Created by hongkailiu on 2015-04-09.
  */
 public class RunListenerActorTest {
 
-    @Before
-    public void setUp() throws Exception {
+    static ActorSystem system;
 
+    @BeforeClass
+    public static void setUpBC() throws Exception {
+        system = ActorSystem.create();
     }
 
-    @After
-    public void tearDown() throws Exception {
-
+    @AfterClass
+    public static void tearDownAC() throws Exception {
+        system.shutdown();
+        system = null;
     }
 
     @Test
     public void testOnReceive() throws Exception {
-        ActorSystem system = ActorSystem.create();
+
         final Props props = Props.create(RunListenerActor.class);
         final TestActorRef<RunListenerActor> ref = TestActorRef.create(system, props, "testB");
         final Future<Object> future = akka.pattern.Patterns.ask(ref, MyMessage.RUN_ON_DELETED, 3000);
