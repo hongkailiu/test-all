3756f0db44a56cf2cce421afa95052feee0b64e3
hongkailiu <liuhkai@gmail.com>
2015-05-21 20:38:05 -0400

    move test-c here

---
+++ trunk/test-c/src/book.cpl/hello.c
@@ -0,0 +1,12 @@
+/*
+ * hello.c
+ *
+ *  Created on: 2015年1月14日
+ *      Author: Liu
+ */
+#include <stdio.h>
+
+main() {
+   printf("Hello, world\n");
+   printf("here i am: \w");
+}
---
+++ trunk/test-c/src/com.hongkailiu.test.c/Hello.c
@@ -0,0 +1,14 @@
+/*
+ * Hello.c
+ *
+ *  Created on: 2015��1��14��
+ *      Author: Liu
+ */
+
+
+#include <stdio.h>
+
+int main1() {
+   printf("Hello, world!\n");
+   return 0;
+}
