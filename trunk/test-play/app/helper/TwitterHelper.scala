5c1bb792e6934d29194a8f6f46cb7211f022bcd6
Hongkai Liu <hongkailiu@users.noreply.github.com>
2015-05-25 14:38:33 -0400

    twitter service: done

--- trunk/test-play/app/helper/TwitterHelper.scala
+++ trunk/test-play/app/helper/TwitterHelper.scala
@@ -1,8 +1,12 @@
 package helper
 
-import java.util.concurrent.{Executors, ExecutorService}
+import java.util.List
+import java.util.concurrent.{ExecutorService, Executors}
 
-import play.api.Logger
+import models.Twitt
+import twitter4j.{Status, QueryResult}
+
+import scala.collection.JavaConversions._
 
 /**
  * Created by ehongka on 5/25/15.
@@ -10,8 +14,42 @@ import play.api.Logger
 object TwitterHelper {
 
   val pool: ExecutorService = Executors.newFixedThreadPool(1)
+  val dwts = "#dwts"
+  val myHandler = new MyHandler
 
-  def start() ={
-    pool.submit(new TwitterService)
+  def start() = {
+
+    pool.submit(new TwitterService(dwts, 3, 10 * 1000, myHandler))
+  }
+}
+
+
+class MyHandler() extends ResultHandler {
+  override def handle(queryResult: QueryResult): Unit = {
+
+    val list: List[Status] = queryResult.getTweets
+    //println(list)
+    if (list!=null){
+      //list.forEach(e => handleStatus(e))
+      //list.foreach(s:Status => println(s))
+      //implicit def javaIteratorToScalaIterator[A](it : java.util.Iterator[A]) = new Wrapper(it)
+      //for (elem:List ) {
+
+      //}
+      for(x <- list) handleStatus(x)
+      //numbers.foreach((i: Int) => i * 2)
+      //}
+    }
+
+  }
+
+  def handleStatus(status:Status): Unit = {
+    println("=====a=======")
+    println(status.getCreatedAt)
+    println(status.getId)
+    println(status.getText)
+    println("=====b=======")
+    //val twitt = new Twitt(status.getCreatedAt.toString, java.lang.Long.toString(status.getId), status.getText)
+    //println(twitt)
   }
 }
--- trunk/test-play/app/helper/TwitterService.scala
+++ trunk/test-play/app/helper/TwitterService.scala
@@ -1,19 +1,34 @@
 package helper
 
 import play.api.Logger
+import twitter4j.{QueryResult, Query, TwitterFactory}
+import twitter4j.conf.ConfigurationBuilder
 
 /**
  * Created by ehongka on 5/25/15.
  */
-class TwitterService extends Runnable {
+class TwitterService(hashTag:String, count:Int, interval: Long , resultHandler: ResultHandler) extends Runnable {
 
+  val cb = new ConfigurationBuilder()
+  cb.setDebugEnabled(true)
+    .setOAuthConsumerKey("uxHMOFhz4LQNzDU1OPl6ICFry")
+    .setOAuthConsumerSecret("p99mPF0nsXW9Gw8BtPWY9CcOuMO9yKG16IenPuBlP0MdcBgMzw")
+    .setOAuthAccessToken("3297829109-CylqTlwceyNEMm72IwcmyBBgAXdvEEwzkvCUxtq")
+    .setOAuthAccessTokenSecret("jpyc5JQHRB47mFDZdxRk13EkZx5clZNBJa7a0LhMLX8XF")
+  val tf = new TwitterFactory(cb.build())
+  val twitter = tf.getInstance()
+  //val dwtsQuery = new Query("#dwts")
+  val dwtsQuery = new Query(hashTag)
 
   def run() {
     try {
       while (true) {
         // TODO
-        Thread sleep 10 * 1000
         Logger.info("start============")
+        dwtsQuery.count(count)
+        val queryResult = twitter.search(dwtsQuery)
+        resultHandler.handle(queryResult)
+        Thread sleep interval
       }
     } catch {
       case e: Exception => {
@@ -23,4 +38,9 @@ class TwitterService extends Runnable {
 
     }
   }
+}
+
+
+trait ResultHandler {
+  def handle(queryResult: QueryResult)
 }
