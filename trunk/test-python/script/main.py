83f44ab49bb26ddc1b37a631df877728734a911a
Hongkai Liu <hongkailiu@users.noreply.github.com>
2015-09-23 16:12:43 -0400

    python basics

---
+++ trunk/test-python/script/main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+import sys; x = 'foo'; sys.stdout.write(x + '\n')
+
+print "Hello, Python!"
+
+if True:
+    print "Answer"
+    print "True"
+else:
+    print "False"
+# print "False"
+
+word = 'word'
+sentence = "This is a sentence."
+paragraph = """This is a paragraph. It is
+made up of multiple lines and sentences."""
+
+total = 1 + \
+        2 + \
+        3
+
+print total
+
+days = ['Monday', 'Tuesday', 'Wednesday',
+        'Thursday', 'Friday']
+
+print days
+
+
+counter = 100          # An integer assignment
+miles = 1000.0       # A floating point
+name = "John"       # A string
+
+print counter
+print miles
+print name
+
+a = b = c = 1
+print a
+print b
+print c
+
+a, b, c = 1, 2, "john"
+print a
+print b
+print c
+
+var1 = 1
+var2 = 10
+print var1
+print var2
+
+del var1, var2
+
+print "a"
+# not defined error if not commented out
+#print var1
+#print var2
+print "b"
---
+++ trunk/test-python/script/test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+print "nihao"
+
+
+raw_input("\n\nPress the enter key to exit.")
