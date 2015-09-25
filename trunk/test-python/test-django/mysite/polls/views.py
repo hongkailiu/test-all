93211214935d4b1bfbedc69d2474c1f940106572
Hongkai Liu <hongkailiu@users.noreply.github.com>
2015-09-25 15:38:59 -0400

    first django app: part 3 finished

---
+++ trunk/test-python/test-django/mysite/polls/views.py
@@ -0,0 +1,28 @@
+# from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from .models import Question
+from django.template import RequestContext, loader
+from django.shortcuts import render
+# Create your views here.
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    # output = ', '.join([p.question_text for p in latest_question_list])
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse(output)
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+    })
+    return HttpResponse(template.render(context))
+
+
+def detail(request, question_id):
+    # return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
